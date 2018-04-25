79517ceced5a66011c7a4a28f0854b7ef2f85d99
sean <seanbruce520@gmail.com>
2018-04-25 14:52:40 +0800

    first improve

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+"use strict";
+
+const path = require('path');
+const Webpack = require('webpack');
+const MiniSccExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+
+const homePage = new HtmlWebpackPlugin({
+  filename: 'taobao.html',
+  template: 'src/taobao.html',
+  chunks: ['app'],
+});
+const supportPlugin = new Webpack.ProvidePlugin({
+});
+const clean = new CleanWebpackPlugin(['dist']);
+const miniSccExtractPlugin = new MiniSccExtractPlugin ({
+  filename: 'app.min.css',
+  outputPath: 'css/',
+  publicPath: 'css/',
+});
+
+const config = {
+  entry: {
+    app: './src/js/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniSccExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: {
+                safe: true
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            }
+          }
+        ],
+        exclude: path.resolve(__dirname, 'src/taobao.html'),
+      }
+    ],
+  },
+  plugins: [
+    supportPlugin,
+    miniSccExtractPlugin,
+    homePage,
+    clean
+  ],
+}
+
+module.exports = config;
