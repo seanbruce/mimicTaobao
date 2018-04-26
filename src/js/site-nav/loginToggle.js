f5f9a85b92a0c95656a5c8ce0714488444a89aed
sean <seanbruce520@gmail.com>
2018-04-26 09:24:02 +0800

    normal progress

--- src/js/site-nav/loginToggle.js
+++ src/js/site-nav/loginToggle.js
@@ -2,9 +2,14 @@
 export function loginToggle() {
   const loginButton = document.querySelector('.site-nav-container-btn-login');
   const siteNavUser = document.getElementById('siteNavUser');
+  const loginStyle_div = loginButton.querySelector('.site-nav-btn-hd');
 
-  loginButton.addEventListener('mouseover', () =>
-    siteNavUser.classList.add('dropdown'), false);
-  loginButton.addEventListener('mouseout', () =>
-    siteNavUser.classList.remove('dropdown'), false);
+  loginButton.addEventListener('mouseover', () => {
+      siteNavUser.classList.add('dropdown');
+      loginStyle_div.classList.add('site-nav-btn-dropdown');
+    }, false);
+  loginButton.addEventListener('mouseout', () => {
+      siteNavUser.classList.remove('dropdown')
+      loginStyle_div.classList.remove('site-nav-btn-dropdown');
+    }, false);
   };
--- src/js/site-nav/regionToggle.js
+++ src/js/site-nav/regionToggle.js
@@ -1,9 +1,14 @@
 export function regionToggle() {
   const regionButton = document.querySelector('.site-nav-container-btn-region');
   const regionList_ul = document.getElementById('siteNavRegionList');
+  const regionstyle_div = regionButton.querySelector('.site-nav-btn-hd');
 
-  regionButton.addEventListener('mouseover', () =>
-    regionList_ul.classList.add('dropdown'), false);
-  regionButton.addEventListener('mouseout', () =>
-    regionList_ul.classList.remove('dropdown'), false);
+  regionButton.addEventListener('mouseover', () => {
+    regionList_ul.classList.add('dropdown');
+    regionstyle_div.classList.add('site-nav-btn-dropdown')
+    }, false);
+  regionButton.addEventListener('mouseout', () => {
+    regionList_ul.classList.remove('dropdown');
+    regionstyle_div.classList.remove('site-nav-btn-dropdown')
+    }, false);
   };
