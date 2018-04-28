48f3a682a98eec2a1ed60eb7c6d3eafc57847add
sean <seanbruce520@gmail.com>
2018-04-28 23:02:01 +0800

    normal

--- src/js/screenOuter/screenOuter.js
+++ src/js/screenOuter/screenOuter.js
@@ -1,12 +1,16 @@
 import {servicesGenerate} from './servicesGenerate.js';
 import {servicesFloatShow} from './servicesFloatShow.js';
 import CarouselOne from './carouselOne.js';
+import CarouselTwo from './carouselTwo.js';
 
 export function screenOuterInit() {
   const carouselOneContainer_div = document.getElementById('carouselOne');
+  const carouselTwoContainer_div = document.getElementById('carouselTwo');
   servicesGenerate();
   servicesFloatShow();
   const carousel1 = new CarouselOne(carouselOneContainer_div, 2000, 2000);
   carousel1.createCarousel();
+  const carousel2 = new CarouselTwo(carouselTwoContainer_div, 2000, 2000);
+  carousel2.createCarousel();
 
 };
